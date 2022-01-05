91de8f6678f67bc4c2e968aa8c9c071b18b6b66a
matthew-halligan <42771186+matthew-halligan@users.noreply.github.com>
2022-01-05 14:11:52 -0500

    remove the sudo call because debian:buster does not recognize

---
+++ Chisel/add_site_cert.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+HOSTNAME=$1
+PORT=$2
+
+TRUST_CERT_FILE_LOC=/etc/ssl/certs/ca-certificates.crt
+
+bash -c "echo -n | openssl s_client -showcerts -connect ${HOSTNAME}:${PORT} \
+2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' \
+>> ${TRUST_CERT_FILE_LOC}"
