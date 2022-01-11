63b96860cf36f2ed601a77bcca22d653466e06cb
kayleecabral <74220069+kayleecabral@users.noreply.github.com>
2022-01-11 15:02:15 -0500

    Update build.sh

--- Chisel/build.sh
+++ Chisel/build.sh
@@ -1,3 +1,9 @@
 #! /bin/sh
 sudo docker build -t chisel .
 docker run --privileged -it chisel
+
+#run chisel with test scripts 
+chisel --build ./test.sh -- make
+
+#HTML visualize 
+chisel --xref ./test.sh file.c
