116014d4e593baa2fbd9c6e423ec77a724ef8d9e
kayleecabral <74220069+kayleecabral@users.noreply.github.com>
2022-03-21 10:36:19 -0400

    Update build.sh

--- Cozart/build.sh
+++ Cozart/build.sh
@@ -1,1 +1,8 @@
 git clone https://github.com/hckuo/Cozart.git
+
+#install dependencies 
+sudo apt install vim kmod qemu qemu-kvm
+
+#build dockerfile
+cd Cozart/docker;
+docker build -f Dockerfile -t cozart-env:latest .
