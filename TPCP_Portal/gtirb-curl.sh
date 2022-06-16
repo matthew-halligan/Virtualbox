d887f49802bca255fe3fa79130631c394283b3c8
Matt <matthew.j.halligan5.civ@us.navy.mil>
2022-06-16 17:54:03 -0400

    Fixing Typo in sed command

--- TPCP_Portal/gtirb-curl.sh
+++ TPCP_Portal/gtirb-curl.sh
@@ -1,2 +1,3 @@
+
 curl -F transform=to-static -F binary=@$(which ls) $(ldd $(which ls) | \
-grep -v vdso | sed 's|^[^/]*/lib|/lib|;s/^[[:space:]]*//;s/.*$//' | sed 's/^/-F library=@/') --output ls.ts.gtirb gtirb/simple
+grep -v vdso | sed 's|^[^/]*/lib|/lib|;s/^[[:space:]]*//;s/ .*$//'|sed 's/^/-F library=@/')  --output ls.ts.gtirb gtirb/simple
