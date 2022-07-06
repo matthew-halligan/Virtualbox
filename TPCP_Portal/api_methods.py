2706b2225f7875da2844c6504fd9ec7434f62b72
Matt <matthew.j.halligan5.civ@us.navy.mil>
2022-07-06 18:30:50 -0400

    Forming a highly specific test case for sort-8.16 to test forming a request to gtirb server

--- TPCP_Portal/api_methods.py
+++ TPCP_Portal/api_methods.py
@@ -1,3 +1,4 @@
+import requests
 import requests as re
 import global_items as gi
 import os
@@ -11,17 +12,50 @@ def get_counter():
     # Task Map Structure is as follows
     # { id:{filename: [filetype, transform, status]}, ...,
     #   id+n:{filename: [filetype, transform, status]} }
-def gtirb_ddisasm(uploads_i_ddisasm):
+def gtirb_ddisasm(uploads_id):
     """
+    Highly specific to sort-8.16 test case
     uploads_id:
 
     """
-    user_space = os.path.join('uploads', str(uploads_i_ddisasm))
-    gtirb_url = "gtirb/simple"
-    # files = {}
-    # for filename in task_map.keys():
-    #     files[filename] = open(os.path.join(user_space, filename), 'rb')
+    # gtirb_url = "gtirb/simple"
+    gtirb_url = 'http://172.20.0.6/simple'.encode("utf8").strip()
+    # binary = 'sort-8.16'
+    # output = binary + ".gtirb"
+    user_space = os.path.join('uploads', str(uploads_id))
+    files = {open("uploads/1/sort-8.16", "rb"),
+             open("/lib/x86_64-linux-gnu/libpthread.so.0", "rb"),
+             open("/lib/x86_64-linux-gnu/libc.so.6", "rb"),
+             open("/lib64/ld-linux-x86-64.so.2", "rb")
+             }
+    # for filename in gi.current_tasks[uploads_id].keys():
+    #     if filename != "JobInfo":
+    #         # print(filename)
+    #         files[filename] = open(os.path.join(user_space, filename), 'rb')
     # file = {filename: open(os.path.join(user_space, filename), 'rb')}
-    raise NotImplementedError
+    # print(file)
 
+    options = {
+        "transform": "ddisasm",
+        "output": "sort-8.16.gtirb"
+    }
+    # print(files)
+    # print(requests.Request("POST", gtirb_url, data=files).prepare().body.decode('utf8'))
+    response = requests.post(gtirb_url, data=options)
+    status = response.status_code
+    print(response.text)
+    print(status)
+    # raise NotImplementedError
+gi.current_tasks = {'1': {'JobInfo': ['Not Specified', 'Must Pipe Output'], 'sort-8.16': ['c/c++ binary', 'Not Specified', '']}}
+gtirb_ddisasm('1')
 
+options = {
+        "transform": "ddisasm",
+        "binary": "sort-8.16",#open("uploads/1/sort-8.16", "rb"),
+        "library": (
+            "libpthread.so.0",
+            "libc.so.6",
+            "ld-linux-x86-64.so.2"
+        ),
+        "output": "sort-8.16.gtirb"
+    }
