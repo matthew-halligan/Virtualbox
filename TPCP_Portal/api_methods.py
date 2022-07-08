0a5dd2154844dcff0dfc9057722543be5b3d05d8
Matt <matthew.j.halligan5.civ@us.navy.mil>
2022-07-08 16:17:30 -0400

    Generated working, highly specific example of HTTP request for gtirb server using ls as example

--- TPCP_Portal/api_methods.py
+++ TPCP_Portal/api_methods.py
@@ -1,7 +1,7 @@
-import requests
 import requests as re
 import global_items as gi
 import os
+import textwrap
 
 
 def get_counter():
@@ -14,20 +14,21 @@ def get_counter():
     #   id+n:{filename: [filetype, transform, status]} }
 def gtirb_ddisasm(uploads_id):
     """
-    Highly specific to sort-8.16 test case
+    Highly specific to 'ls' test case
     uploads_id:
 
     """
-    # gtirb_url = "gtirb/simple"
     gtirb_url = 'http://172.20.0.6/simple'.encode("utf8").strip()
-    # binary = 'sort-8.16'
-    # output = binary + ".gtirb"
+
     user_space = os.path.join('uploads', str(uploads_id))
-    files = {open("uploads/1/sort-8.16", "rb"),
-             open("/lib/x86_64-linux-gnu/libpthread.so.0", "rb"),
-             open("/lib/x86_64-linux-gnu/libc.so.6", "rb"),
-             open("/lib64/ld-linux-x86-64.so.2", "rb")
-             }
+    files = [("binary", ("ls", open("/bin/ls", "rb"))),
+             ("library", ("libpthread.so.0", open("/lib/x86_64-linux-gnu/libpthread.so.0", "rb"))),
+             ("library", ("libc.so.6", open("/lib/x86_64-linux-gnu/libc.so.6", "rb"))),
+             ("library", ("ld-linux-x86-64.so.2", open("/lib64/ld-linux-x86-64.so.2", "rb"))),
+             ("library", ("libselinux.so.1", open("/lib/x86_64-linux-gnu/libselinux.so.1", "rb"))),
+             ("library", ("libpcre2-8.so.0", open("/lib/x86_64-linux-gnu/libpcre2-8.so.0", "rb"))),
+             ("library", ("libdl.so.2", open("/lib/x86_64-linux-gnu/libdl.so.2", "rb")))
+             ]
     # for filename in gi.current_tasks[uploads_id].keys():
     #     if filename != "JobInfo":
     #         # print(filename)
@@ -35,27 +36,40 @@ def gtirb_ddisasm(uploads_id):
     # file = {filename: open(os.path.join(user_space, filename), 'rb')}
     # print(file)
 
-    options = {
-        "transform": "ddisasm",
-        "output": "sort-8.16.gtirb"
-    }
-    # print(files)
-    # print(requests.Request("POST", gtirb_url, data=files).prepare().body.decode('utf8'))
-    response = requests.post(gtirb_url, data=options)
+
+    response = re.post(gtirb_url, files=files, data={"transform": "to-static,stack-stamp,pretty-print"})
     status = response.status_code
     print(response.text)
     print(status)
-    # raise NotImplementedError
+
+    # req = requests.Request('POST', gtirb_url, files=files, data={"transform": "ddisasm"})
+    # prepared = req.prepare()
+
+    # print(ascii(prepared.body))
+    # pretty_print_POST(prepared)
+
+    # Write output of response body to the desired output location
+    # with open(output_location, "wb") as ol:
+    #     ol.write(res.body)
+
+def pretty_print_POST(req):
+    """
+    At this point it is completely built and ready
+    to be fired; it is "prepared".
+
+    However pay attention at the formatting used in
+    this function because it is programmed to be pretty
+    printed and may differ from the actual request.
+    """
+    text = '{}\n{}\r\n{}\r\n\r\n{}'.format(
+        '-----------START-----------',
+        req.method + ' ' + req.url,
+        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+        req.body,
+    )
+
+    print(textwrap.fill(text, width=80))
+
+
 gi.current_tasks = {'1': {'JobInfo': ['Not Specified', 'Must Pipe Output'], 'sort-8.16': ['c/c++ binary', 'Not Specified', '']}}
 gtirb_ddisasm('1')
-
-options = {
-        "transform": "ddisasm",
-        "binary": "sort-8.16",#open("uploads/1/sort-8.16", "rb"),
-        "library": (
-            "libpthread.so.0",
-            "libc.so.6",
-            "ld-linux-x86-64.so.2"
-        ),
-        "output": "sort-8.16.gtirb"
-    }
