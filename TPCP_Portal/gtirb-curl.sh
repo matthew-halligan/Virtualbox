ebdc5929824ff071171aac49da04d04a07b352a4
Matt <matthew.j.halligan5.civ@us.navy.mil>
2022-07-14 12:12:28 -0400

    testing GTIRB server on static binary print for ls example

--- TPCP_Portal/gtirb-curl.sh
+++ TPCP_Portal/gtirb-curl.sh
@@ -1,3 +1,18 @@
-curl -F transform=to-static -F binary=@$(which ls) $(ldd $(which ls) | \
+#curl -F transform=ddisasm -F binary=@$(which ls) $(ldd $(which ls) | \
+#grep -v vdso | sed 's|^[^/]*/lib|/lib|;s/^[[:space:]]*//;s/ .*$//'| \
+#sed 's/^/-F library=@/')  --output ls.ts.gtirb http://172.20.0.6/simple --trace-ascii -
+
+
+#curl -F transform=ddisasm,binary-print -F binary=@$(which ls) --output ls.ts.gtirb http://172.17.0.1/simple
+
+
+#$(ldd $(which ls) | \
+#grep -v vdso | sed 's|^[^/]*/lib|/lib|;s/^[[:space:]]*//;s/ .*$//'| \
+#sed 's/^/-F library=@/')
+
+curl -F transform=to-static,stack-stamp -F binary=@$(which ls) $(ldd $(which ls) | \
 grep -v vdso | sed 's|^[^/]*/lib|/lib|;s/^[[:space:]]*//;s/ .*$//'| \
-sed 's/^/-F library=@/')  --output ls.ts.gtirb http://172.20.0.6/simple --trace-ascii -
+sed 's/^/-F library=@/')  --output ls.tss.gtirb http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' gtirb)/simple
+
+sudo chmod 777 ls.tss.gtirb
+sudo curl -F transform=static-binary-print -F gtirb=@./ls.tss.gtirb $(ldd $(which ls) | grep -v vdso | sed 's|^[^/]*/lib|/lib|;s/^[[:space:]]*//;s/ .*$//'| sed 's/^/-F library=@/') --output ls.tss.sbp http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' gtirb)/simple
