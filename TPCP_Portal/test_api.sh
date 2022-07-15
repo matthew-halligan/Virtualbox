b920d066fcdb8f72f1bf91761b2236609502d42f
Matt <matthew.j.halligan5.civ@us.navy.mil>
2022-07-15 13:57:40 -0400

    Removed originals in favor of placing inside TPCP_Portal

--- TPCP_Portal/test_api.sh
+++
@@ -1,5 +0,0 @@
-python api_methods.py > log-flaskgtirb.log
-echo \n\n "##########Flask GTIRB##############"
-head log-flaskgtirb.log
-echo \n\n "##########CURL GTIRB###############"
-head -n 30 log-gtirbconnect.log
