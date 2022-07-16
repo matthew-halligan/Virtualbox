8d3d9079696fbe2f6957099eb490df6e46c2a4fa
Justin Cabral <ijustincabral@gmail.com>
2022-07-16 22:26:15 +0000

    GSA Server/Client revisions

--- TPCP_Portal/gsa-server/server.py
+++ TPCP_Portal/gsa-server/server.py
@@ -3,7 +3,7 @@ import os
 
 HOST = "172.20.0.8" #This would be the server IP based on the docker-compose.yml
 PORT = 6000 #This would be the exposed port that we're communicating on
-BUFFER_SIZE = 1 #In bytes (Only need 1 byte for indexes 0-255. Small buffer = fast read/writes)
+BUFFER_SIZE = 1024 #In bytes (Figure out what to limit to later. Small buffer = fast read/writes)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -21,14 +21,20 @@ while True:
         if not data: break
         print (f"Data Received: {data}"),data
         print("")
+
         decoded_data = data.decode("utf-8")
-        print(f"Data Decoded: {decoded_data}")
+        strs = decoded_data.split(",")
+        index = strs[0]
+        binary = strs[1]
+        transformedBinary = strs[2]
+
+        print(f"Data Decoded: {index} {binary} {transformedBinary}")
         print("")
-        print(f"-=== Running GSA for binaries in /uploads/{decoded_data} ===-")
+        print(f"-=== Running GSA for binaries in /uploads/{index} ===-")
         print("")
 
         #Tell container to run the GSA with the passed index
-        os.system(f"python3 server/run_gsa.py {decoded_data}")
+        os.system(f"python3 server/run_gsa.py {index} {binary} {transformedBinary}")
         conn.send(data)
 
     conn.close()
