5ef31f32e5fa3a6646f147deb16d347592483df9
Matt <matthew.j.halligan5.civ@us.navy.mil>
2022-07-29 12:44:34 -0400

    exported BUILD_ENV for quick method of determining if prod or dev is running

--- TPCP_Portal/env-switch.sh
+++ TPCP_Portal/env-switch.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
 docker_env=$1
-echo $docker_env
-echo "prod"
+
 if [ ! -f  "docker-compose.yml" ]; then
   echo "docker-compose does not exist";
   touch docker-compose.yml;
@@ -10,10 +9,12 @@ fi
 
 if [ $docker_env == "prod" ]; then
   echo "switching to prod"
+  export BUILD_ENV=$(docker_env)
   sudo cp docker-compose-prod.yml docker-compose.yml
 fi
 
 if [ $docker_env == "dev" ]; then
   echo "switching to dev"
+  export BUILD_ENV=$(docker_env)
   sudo cp docker-compose-dev.yml docker-compose.yml
 fi
